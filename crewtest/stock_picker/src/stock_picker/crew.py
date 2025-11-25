55d609a8174c4878b7f157ead0781b36abc91264
maria-shpatserman <sokunova@gmail.com>
2025-11-25 08:50:09 +0800

    create new ptoject stock_picker

---
+++ crewtest/stock_picker/src/stock_picker/crew.py
@@ -0,0 +1,15 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
+
+
+@CrewBase
+class StockPicker():
+    """StockPicker crew"""
+
+    agents_config = 'config/agents.yaml'
+    tasks_config = 'config/tasks.yaml'
+    
+
+    
